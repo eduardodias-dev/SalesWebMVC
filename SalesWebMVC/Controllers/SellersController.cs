dcfc5340c1f4dadfe731ac72e64beb013e9e96e8
Eduardo Dias <mr.diasjr@gmail.com>
2020-09-08 21:46:26 -0300

    Seller Service and basic findAll

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using SalesWebMVC.Services;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        //injeção de dependência
+        private readonly SellerService _sellerService;
+
+        //Como esta classe está inserida como parte do escopo, o sellerService já é instanciado com o context;
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+        public IActionResult Index()
+        {
+            List<Seller> list = _sellerService.FindAll();
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        //Injeção de dependência
+        private readonly SalesWebMVCContext _context;
+
+        //Ao colocar o context como parâmetro do construtor, o Startup da aplicação irá inserir o context para todos os que estiverem como AddScoped
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
